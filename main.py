f807aa01e7489a0b2db2e01d08db39e92d814444
giseldo <giseldo@gmail.com>
2019-09-11 15:38:07 -0300

    incluido recurso de sessão

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+
 import aiml
+from random import *
 
 app = Flask(__name__)
-
+app.secret_key = 'any random string'
 
 kernel = aiml.Kernel()
 kernel.learn("aiml/config.xml")
@@ -11,13 +13,19 @@ kernel.respond("CEREBRO")
 
 @app.route("/")
 def home():
+    sessionid = randint(100,1000)
+    session['sessionid'] = sessionid
+    print("home: {}".format(sessionid))
     return render_template("index.html")
 
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    saida = kernel.respond(userText)
+    print("userText: {}".format(userText))
+    sessionid = session.get('sessionid')
+    print("get: {}".format(sessionid))
+    saida = kernel.respond(userText, sessionid)
     return str(saida)
 
 

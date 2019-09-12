b5df456f4eb8df76066c93e4b77da1e421bf3aa8
giseldo <giseldo@gmail.com>
2019-09-11 21:54:50 -0300

    inclusao do brain

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, session
 
 import aiml
+import os
 from random import *
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
 kernel = aiml.Kernel()
-kernel.learn("aiml/config.xml")
-kernel.respond("CEREBRO")
+#kernel.learn("aiml/config.xml")
+#kernel.respond("CEREBRO")
 #kernel.loadSubs(filename = "aiml/substituicao.ini")
 
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "aiml/config.xml", commands = "CEREBRO")
+    kernel.saveBrain("bot_brain.brn")
 
 @app.route("/")
 def home():

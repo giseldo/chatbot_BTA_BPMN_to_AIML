e4a593b388edf2e53f79f9a1edde83186e800fdf
giseldo <giseldo@gmail.com>
2019-09-14 01:11:01 -0300

    tentativa de correcao bug

--- main.py
+++ main.py
@@ -26,8 +26,20 @@ def get_bot_response():
     saida = botty.respond(entrada, sessionid)
     db.inserir(sessionid, entrada, saida)
     if saida == '':
-        saida = "Não entendi."
+        saida = "Nao entendi."
     return str(saida)
+
+@app.route('/home')
+def index():
+    return render_template('index_novo.html')
+
+@app.route('/process',methods=['POST'])
+def process():
+    user_input=request.form['user_input']
+    sessionid = session.get('sessionid')
+    bot_response = botty.respond(user_input, sessionid)
+    return render_template('index_novo.html', user_input=user_input, bot_response=bot_response)
+
 
 
 if __name__ == "__main__":

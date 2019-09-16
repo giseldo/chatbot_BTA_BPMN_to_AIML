08b8507b6607f24e5525a44ca789495cee05a28b
giseldo <giseldo@gmail.com>
2019-09-16 17:08:28 -0300

    tentativa correcao bug

--- botty.py
+++ botty.py
@@ -1,14 +1,15 @@
 import aiml
 import os
 
+class botty():
+    kernel = None
+    def __init__(self):
+        self.kernel = aiml.Kernel()
+        if os.path.isfile("bot_brain.brn"):
+            self.kernel.bootstrap(brainFile="bot_brain.brn")
+        else:
+            self.kernel.bootstrap(learnFiles="aiml/config.xml", commands="CEREBRO")
+            self.kernel.saveBrain("bot_brain.brn")
 
-kernel = aiml.Kernel()
-if os.path.isfile("bot_brain.brn"):
-    kernel.bootstrap(brainFile="bot_brain.brn")
-else:
-    kernel.bootstrap(learnFiles="aiml/config.xml", commands="CEREBRO")
-    kernel.saveBrain("bot_brain.brn")
-
-
-def respond(entrada, sessionid):
-    return kernel.respond(entrada, sessionid)
+    def responder_novo(entrada_, sessionid_):
+        return self.kernel.respond(input_= entrada_, sessionID = sessionid_)
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
 from random import *
-import botty
 import db
-
+from botty import botty
+from werkzeug.wsgi import responder
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
 Session(app)
-
+b = botty() 
 
 @app.route("/")
-def home():
+def index():
     sessionid = randint(1000,10000)
     session['sessionid'] = sessionid
     return render_template("index.html")
@@ -23,21 +23,25 @@ def home():
 def get_bot_response():
     entrada = request.args.get('msg')
     sessionid = session.get('sessionid')
-    saida = botty.respond(entrada, sessionid)
-    db.inserir(sessionid, entrada, saida)
-    if saida == '':
+    print(entrada)
+    print(sessionid)
+    print("isto e um teste")
+    saida = b.responder_novo(entrada, sessionid)
+    saida = ""
+    if saida == "":
         saida = "Nao entendi."
     return str(saida)
 
+
 @app.route('/home')
-def index():
-    return render_template('index_novo.html')
+def home():
+    return render_template('index.html')
 
 @app.route('/process',methods=['POST'])
 def process():
     user_input=request.form['user_input']
     sessionid = session.get('sessionid')
-    bot_response = botty.respond(user_input, sessionid)
+    bot_response = b.respond(user_input, sessionid)
     return render_template('index_novo.html', user_input=user_input, bot_response=bot_response)
 
 

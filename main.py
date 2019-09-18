b3efd0cbb85b2f57d99b9ef5b15c6d126aec7c37
giseldo <giseldo@gmail.com>
2019-09-18 14:43:06 -0300

    inclusao rest

--- main.py
+++ main.py
@@ -2,13 +2,52 @@
 
 from flask import Flask, render_template, request, session
 from random import *
-from MockArgumentsGiseldo import MockArgumentParserGiseldo
 from programy.utils.logging.ylogger import YLogger
 from programy.clients.events.client import EventBotClient
 from programy.clients.events.console.config import ConsoleConfiguration
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
+
+
+class MockArgumentsGiseldo(object):
+
+    def __init__(self,
+                    bot_root = ".",
+                    logging = None,
+                    config  = 'config.yaml',
+                    cformat = "yaml",
+                    noloop = False,
+                    substitutions='subs.txt'
+                ):
+        self.bot_root = bot_root
+        self.logging = logging
+        self.config = config
+        self.cformat = cformat
+        self.noloop = noloop
+        self.substitutions = substitutions
+
+
+class MockArgumentParserGiseldo(object):
+
+    def add_argument(self, argument, dest=None, action=None, help=None):
+        pass
+
+    def parse_args(self):
+        return MockArgumentsGiseldo()
+
+
+class MockClientGiseldo(object):
+
+    def get_description(self):
+        return "MockClient"
+
+    def add_client_arguments(self, parser):
+        pass
+
+    def parse_args(sel, arguments, parsed_args):
+        pass
+
 
 
 class ConsoleBotClientGiseldo(EventBotClient):

fa6634c85a2c9ea4b4f241a47c1651c7919064fe
giseldo <giseldo@gmail.com>
2019-09-18 11:13:23 -0300

    correacao app

--- programy/clients/restful/flask/client.py
+++ programy/clients/restful/flask/client.py
@@ -76,18 +76,18 @@ if __name__ == '__main__':
     REST_CLIENT = None
 
     print("Initiating Flask REST Service...")
-    APP = Flask(__name__)
+    app = Flask(__name__)
 
-    @APP.route('/api/rest/v1.0/ask', methods=['GET', 'POST'])
+    @app.route('/api/rest/v1.0/ask', methods=['GET', 'POST'])
     def ask_v1_0():
         response_data, status = REST_CLIENT.process_request(request, version=1.0)
         return REST_CLIENT.create_response(response_data, status, version=1.0)
 
-    @APP.route('/api/rest/v2.0/ask', methods=['GET', 'POST'])
+    @app.route('/api/rest/v2.0/ask', methods=['GET', 'POST'])
     def ask_v2_0():
         response_data, status = REST_CLIENT.process_request(request, version=2.0)
         return REST_CLIENT.create_response(response_data, status, version=2.0)
 
     print("Loading, please wait...")
     REST_CLIENT = FlaskRestBotClient("flask")
-    REST_CLIENT.run(APP)
+    REST_CLIENT.run(app)

f0af65d3c5901631d58c3561ad598f3189e5a478
giseldo <giseldo@gmail.com>
2019-09-18 15:17:31 -0300

    alteracao flas

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template, request, session
-from programy.utils.logging.ylogger import YLogger
-from programy.clients.events.client import EventBotClient
-from programy.clients.events.console.config import ConsoleConfiguration
-from flask import Flask, jsonify, request, make_response, abort, Response
 from programy.clients.restful.client import RestBotClient
+from flask import Flask, jsonify, request, make_response, abort, Response, render_template
+
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
@@ -69,6 +66,7 @@ class FlaskRestBotClient(RestBotClient):
         else:
             return make_response('Invalid API version', 400)
 
+
     def run(self, flask):
 
         print("%s Client running on http://%s:%s" % (self.id, self.configuration.client_configuration.host,
@@ -96,65 +94,6 @@ class FlaskRestBotClient(RestBotClient):
                       debug=self.configuration.client_configuration.debug)
 
         self.shutdown()
-
-
-class ConsoleBotClientGiseldo(EventBotClient):
-
-    def __init__(self, argument_parser=None):
-        self.running = False
-        c = MockArgumentParserGiseldo()
-        EventBotClient.__init__(self, "Console", c)
-
-    def get_client_configuration(self):
-        return ConsoleConfiguration()
-
-    def add_client_arguments(self, parser=None):
-        return
-
-    def parse_args(self, arguments, parsed_args):
-        return
-
-    def get_question(self, client_context, input_func=input):
-        ask = "%s " % self.get_client_configuration().prompt
-        return input_func(ask)
-
-    def display_startup_messages(self, client_context):
-        self.process_response(client_context, client_context.bot.get_version_string(client_context))
-        initial_question = client_context.bot.get_initial_question(client_context)
-        self._renderer.render(client_context, initial_question)
-
-    def process_question(self, client_context, question):
-        self._questions += 1
-        return client_context.bot.ask_question(client_context , question, responselogger=self)
-
-    def render_response(self, client_context, response):
-        # Calls the renderer which handles RCS context, and then calls back to the client to show response
-        self._renderer.render(client_context, response)
-
-    def process_response(self, client_context, response):
-        print(response)
-
-    def process_question_answer(self, client_context):
-        question = self.get_question(client_context)
-        response = self.process_question(client_context, question)
-        self.render_response(client_context, response)
-
-    def wait_and_answer(self):
-        running = True
-        try:
-            client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
-            self.process_question_answer(client_context)
-        except KeyboardInterrupt as keye:
-            running = False
-            client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
-            self._renderer.render(client_context, client_context.bot.get_exit_response(client_context))
-        except Exception as excep:
-            YLogger.exception(self, "Oops something bad happened !", excep)
-        return running
-
-    def prior_to_run_loop(self):
-        client_context = self.create_client_context(self._configuration.client_configuration.default_userid)
-        self.display_startup_messages(client_context)
 
 
 if __name__ == '__main__':

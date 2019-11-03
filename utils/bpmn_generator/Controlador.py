5c41804a88ab4a57e47c084548d84b8e34410075
Giseldo da Silva Neo <giseldo@gmail.com>
2019-11-03 19:52:24 -0300

    refatoramento

---
+++ utils/bpmn_generator/Controlador.py
@@ -0,0 +1,37 @@
+from AIML_ import AIMLClass
+from BPMN_ import BPMNClass
+import xml.etree.ElementTree as ET
+
+class Controlador:
+    def generateAIML(self, inputBPMNFileName):
+        ### generate necessary variables for integration from BPMN
+        bpmnObject = BPMNClass()
+        rootBPMN = bpmnObject.getRootBPMNFromFile(inputBPMNFileName)
+        templateName, pattern_event_init_value = bpmnObject.getTemplateName(rootBPMN)
+        firstActivityName = bpmnObject.get_first_activity_name(rootBPMN)
+        activityOne = bpmnObject.get_activityes(rootBPMN)[1].attrib['name']
+        activityTwo = bpmnObject.get_activityes(rootBPMN)[2].attrib['name']
+        ## generate aiml from bpmn using generated variables
+        aimlObject = AIMLClass()
+        rootAIML = aimlObject.addCategoryInit(pattern_event_init_value, templateName)
+        topicAIML = aimlObject.addTopic(rootAIML, templateName)
+        aimlObject.addCategoryTopic(topicAIML, 'STEP_' + str(pattern_event_init_value).upper(), firstActivityName)
+        aimlObject.addCategoryFlow(topicAIML, '*', firstActivityName, 'resultado', 'sim', 'STEP_SIM','nao', 'STEP_NAO')
+        aimlObject.addCategoryTopic(topicAIML, 'STEP_SIM', activityOne)
+        aimlObject.addCategoryTopic(topicAIML, 'STEP_NAO', activityTwo)
+        return rootAIML
+
+    def outputBPMNFile(self, filename, rootAIML):
+        file = open(filename, 'wb')
+        tree = ET.ElementTree(rootAIML)
+        tree.write(file)
+        file.close()
+
+
+if __name__ == '__main__':
+    ### Integration between BPMN and AIML
+    controlador = Controlador()
+    rootAIML = controlador.generateAIML('input/carro.bpmn')
+    ### Generate AIML file
+    controlador.outputBPMNFile('output/carro.aiml', rootAIML)
+    controlador.outputBPMNFile('../../storagedepressao/categories/ari/carro.aiml', rootAIML)
--- utils/bpmn_generator/bpmn2aiml.py
+++ utils/bpmn_generator/bpmn2aiml.py
@@ -1,53 +1,59 @@
 import xml.etree.ElementTree as ET
 
-templateName = ""
 
-treeInput = ET.parse("input/pmbok.bpmn")
-
-rootDefinition = treeInput.getroot()
-definitionTagName = rootDefinition.tag
-# templateName = process.ID
-# pattern_event_init_value = startEvent.Name
-for process in rootDefinition:
-    if "process" in process.tag:
-        templateName = process.attrib['id']
-        for start in process:
-            if "startEvent" in start.tag:
-                pattern_event_init_value = startEventName = start.attrib['name']
+############################# LOAD FILE #############################
 
 
-def get_init_id(root):
+def getRootBPMNFromFile(inputFileName):
+    treeInput = ET.parse(inputFileName)
+    rootBPMN = treeInput.getroot()
+    return rootBPMN
+
+
+def getTemplateName(rootBPMN):
+    for process in rootBPMN:
+        if "process" in process.tag:
+            templateName = process.attrib['id']
+            for start in process:
+                if "startEvent" in start.tag:
+                    pattern_event_init_value = start.attrib['name']
+    return templateName, pattern_event_init_value
+
+
+def get_init_id(rootBPMN):
     seach_tag = "./{http://www.omg.org/spec/BPMN/20100524/MODEL}process/{http://www.omg.org/spec/BPMN/20100524/MODEL}startEvent"
-    for startEvent in root.findall(seach_tag):
+    for startEvent in rootBPMN.findall(seach_tag):
         return startEvent.attrib['id']
 
 
 # get first activity
-def get_first_activity_name(root):
+def get_first_activity_name(rootBPMN):
     seach_tag = "./{http://www.omg.org/spec/BPMN/20100524/MODEL}process/{http://www.omg.org/spec/BPMN/20100524/MODEL}sequenceFlow"
-    for sequenceFlow in root.findall(seach_tag):
-        if sequenceFlow.attrib['sourceRef'] == get_init_id(root):
+    for sequenceFlow in rootBPMN.findall(seach_tag):
+        if sequenceFlow.attrib['sourceRef'] == get_init_id(rootBPMN):
             next_task_id = sequenceFlow.attrib['targetRef']
 
     seach_tag_service_task = "./{http://www.omg.org/spec/BPMN/20100524/MODEL}process/{http://www.omg.org/spec/BPMN/20100524/MODEL}serviceTask"
-    for serviceTask in root.findall(seach_tag_service_task):
+    for serviceTask in rootBPMN.findall(seach_tag_service_task):
         if serviceTask.attrib['id'] == next_task_id:
             return serviceTask.attrib['name']
 
 
 # get first activity
-def get_sequeceFlows(root):
+def get_sequeceFlows(rootBPMN):
     seach_tag = "./{http://www.omg.org/spec/BPMN/20100524/MODEL}process/{http://www.omg.org/spec/BPMN/20100524/MODEL}sequenceFlow"
-    for sequenceFlow in root.findall(seach_tag):
+    for sequenceFlow in rootBPMN.findall(seach_tag):
         return sequenceFlow
 # print(get_sequeceFlows(rootDefinition))
 
 # get first activity
-def get_activityes(root):
+def get_activityes(rootBPMN):
     seach_tag = "./{http://www.omg.org/spec/BPMN/20100524/MODEL}process/{http://www.omg.org/spec/BPMN/20100524/MODEL}serviceTask"
-    return root.findall(seach_tag)
+    return rootBPMN.findall(seach_tag)
 # print(get_activityes(rootDefinition))
 
+
+############################################### CREATE FILE #############################
 
 
 def addCategoryTopic(root, patternText, templateText):
@@ -58,23 +64,28 @@ def addCategoryTopic(root, patternText, templateText):
     template.text = templateText
 
 
-# inicio das tags
-aiml = ET.Element('aiml')
-category = ET.SubElement(aiml, 'category')
-pattern = ET.SubElement(category, 'pattern')
-pattern.text = str(pattern_event_init_value).upper()
-template = ET.SubElement(category, 'template')
-think = ET.SubElement(template, 'think')
-set = ET.SubElement(think, 'set')
-set.text = str(templateName).upper()
-set.attrib = {'name': 'topic'}
-srai = ET.SubElement(template,'srai')
-srai.text = 'STEP_' + str(pattern_event_init_value).upper()
+def addCategoryInit(pattern_event_init_value, templateName):
+    # inicio das tags
+    rootAIML = ET.Element('aiml')
+    category = ET.SubElement(rootAIML, 'category')
+    pattern = ET.SubElement(category, 'pattern')
+    pattern.text = str(pattern_event_init_value).upper()
+    template = ET.SubElement(category, 'template')
+    think = ET.SubElement(template, 'think')
+    set = ET.SubElement(think, 'set')
+    set.text = str(templateName).upper()
+    set.attrib = {'name': 'topic'}
+    srai = ET.SubElement(template,'srai')
+    srai.text = 'STEP_' + str(pattern_event_init_value).upper()
+
+    return rootAIML
 
 
-# inicio das tags topic
-topic = ET.SubElement(aiml,"topic")
-topic.attrib['name'] = str(templateName).upper()
+def addTopic(rootAIML, templateName):
+    # inicio das tags topic
+    topic = ET.SubElement(rootAIML,"topic")
+    topic.attrib['name'] = str(templateName).upper()
+    return topic
 
 
 def addCategoryFlow(root, patternValue, templateValue, varname, optionKeyA, optionValueA, optionKeyB, optionValueB):
@@ -102,17 +113,36 @@ def addCategoryFlow(root, patternValue, templateValue, varname, optionKeyA, opti
     sraitopic2.text = optionValueB
 
 
-addCategoryTopic(topic, 'STEP_' + str(pattern_event_init_value).upper(), get_first_activity_name(rootDefinition))
-addCategoryFlow(topic, '*', get_first_activity_name(rootDefinition), 'resultado', 'sim', 'STEP_SIM','nao', 'STEP_NAO')
-addCategoryTopic(topic ,'STEP_SIM', get_activityes(rootDefinition)[1].attrib['name'])
-addCategoryTopic(topic ,'STEP_NAO', get_activityes(rootDefinition)[2].attrib['name'])
+def outputBPMNFile(filename, rootAIML):
+    file = open(filename, 'wb')
+    tree = ET.ElementTree(rootAIML)
+    tree.write(file)
+    file.close()
 
-file = open('output/pmbok.aiml', 'wb')
-tree = ET.ElementTree(aiml)
-tree.write(file)
-file.close()
+### controlador
 
-file = open('../../storagedepressao/categories/ari/pmbok.aiml', 'wb')
-tree = ET.ElementTree(aiml)
-tree.write(file)
-file.close()
+def generateAIML(rootBPMN):
+    ### generate necessary variables for integration
+    templateName, pattern_event_init_value = getTemplateName(rootBPMN)
+    firstActivityName = get_first_activity_name(rootBPMN)
+    activityOne = get_activityes(rootBPMN)[1].attrib['name']
+    activityTwo = get_activityes(rootBPMN)[2].attrib['name']
+    ## generate aiml from bpmn using generated variables
+    rootAIML = addCategoryInit(pattern_event_init_value, templateName)
+    topicAIML = addTopic(rootAIML, templateName)
+    addCategoryTopic(topicAIML, 'STEP_' + str(pattern_event_init_value).upper(), firstActivityName)
+    addCategoryFlow(topicAIML, '*', firstActivityName, 'resultado', 'sim', 'STEP_SIM','nao', 'STEP_NAO')
+    addCategoryTopic(topicAIML, 'STEP_SIM', activityOne)
+    addCategoryTopic(topicAIML, 'STEP_NAO', activityTwo)
+    return rootAIML
+
+### principal
+
+if __name__ == '__main__':
+    ### Carrega BPMN
+    rootBPMN = getRootBPMNFromFile('input/carro.bpmn')
+    ### Integration between BPMN and AIML
+    rootAIML = generateAIML(rootBPMN)
+    ### Generate AIML file
+    outputBPMNFile('output/carro.aiml', rootAIML)
+    outputBPMNFile('../../storagedepressao/categories/ari/carro.aiml', rootAIML)

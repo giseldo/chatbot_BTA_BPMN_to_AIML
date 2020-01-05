f3a9e1074f8aae7f8a9a8d9a5b36e387e6ab6f00
giseldo <giseldo@gmail.com>
2020-01-04 22:33:49 -0300

    ultima versao

--- clientweb.py
+++ clientweb.py
@@ -251,4 +251,5 @@ def delete_files():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run()
+    # app.run(host='127.0.0.1')
--- enviar.bat
+++ enviar.bat
@@ -1,3 +1,4 @@
 git add .
 git commit -m "ultima versao"
 git push origin master
+
